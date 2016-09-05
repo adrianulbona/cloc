f2ccd0951e873771eb751a51a40acd6b52601419
Adrian Bona <adrianulbona@gmail.com>
2016-09-05 19:11:56 +0300

    Add granularity five file

--- src/main/java/io/github/adrianulbona/cloc/GeoDeliveryStreamer.java
+++ src/main/java/io/github/adrianulbona/cloc/GeoDeliveryStreamer.java
@@ -1,11 +1,15 @@
 package io.github.adrianulbona.cloc;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.zip.GZIPInputStream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -16,7 +20,10 @@ public class GeoDeliveryStreamer {
 
 
 	public Stream<GeoDelivery<String>> with(Granularity granularity) throws IOException {
-		return Files.lines(granularity.path).map(this::parse);
+		final GZIPInputStream gzip = new GZIPInputStream(Files.newInputStream(granularity.path));
+		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(gzip))) {
+			return reader.lines().collect(toList()).stream().map(this::parse);
+		}
 	}
 
 	private GeoDelivery<String> parse(String rawGeoUnit) {
@@ -28,13 +35,16 @@ public class GeoDeliveryStreamer {
 	}
 
 	public enum Granularity {
-		FIVE(Paths.get("data/cloc5.csv")),
-		SIX(Paths.get("data/cloc6.csv"));
+		FIVE("cloc5.csv.gz");
 
-		private final Path path;
+		public final Path path;
 
-		Granularity(Path path) {
-			this.path = path;
+		Granularity(String path) {
+			try {
+				this.path = Paths.get(GeoDeliveryStreamer.class.getResource(path).toURI().getPath());
+			} catch (URISyntaxException e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 }
--- src/test/java/io/github/adrianulbona/cloc/GeoDeliveryStreamerTest.java
+++ src/test/java/io/github/adrianulbona/cloc/GeoDeliveryStreamerTest.java
@@ -12,24 +12,24 @@ public class GeoDeliveryStreamerTest {
 
 	@Test
 	public void testNotNull() throws Exception {
-		assertNotNull(new GeoDeliveryStreamer().with(Granularity.SIX));
+		assertNotNull(new GeoDeliveryStreamer().with(Granularity.FIVE));
 	}
 
 	@Test
 	public void testCount() throws Exception {
-		assertEquals(9505218, new GeoDeliveryStreamer().with(Granularity.SIX).count());
+		assertEquals(1259238, new GeoDeliveryStreamer().with(Granularity.FIVE).count());
 	}
 
 	@Test
 	public void testValidCountry() throws Exception {
-		new GeoDeliveryStreamer().with(Granularity.SIX).forEach(geoHashCountry -> {
+		new GeoDeliveryStreamer().with(Granularity.FIVE).forEach(geoHashCountry -> {
 			assertNotNull(geoHashCountry.getPack());
 		});
 	}
 
 	@Test
 	public void testValidGeoHash() throws Exception {
-		new GeoDeliveryStreamer().with(Granularity.SIX).forEach(geoHashCountry -> {
+		new GeoDeliveryStreamer().with(Granularity.FIVE).forEach(geoHashCountry -> {
 			assertNotNull(geoHashCountry.getPath());
 			assertFalse(geoHashCountry.getPath().isEmpty());
 		});
