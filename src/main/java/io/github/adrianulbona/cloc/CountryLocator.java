e01164bdaa53c02b471c89e75c9c3bdf057388d2
Adrian Bona <adrianulbona@gmail.com>
2016-10-30 16:31:34 +0200

    Prepare pom for Maven Central release

--- src/main/java/io/github/adrianulbona/cloc/CountryLocator.java
+++ src/main/java/io/github/adrianulbona/cloc/CountryLocator.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
